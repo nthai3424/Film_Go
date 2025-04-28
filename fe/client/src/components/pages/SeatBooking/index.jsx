fd3cae606e91b74db5d0b6de40c1bc738ae455a0
nthai3424 <thainguyen3424@gmail.com>
2025-04-28 09:46:43 +0700

    update status chair

--- fe/client/src/components/pages/SeatBooking/index.jsx
+++ fe/client/src/components/pages/SeatBooking/index.jsx
@@ -503,7 +503,7 @@ const SeatBooking = () => {
                             <div className="flex justify-center items-end gap-[10px]">
                                 <p className="uppercase text-[20px] font-[400]">{movie?.name}</p>
                                 <p className="uppercase text-[20px] font-[400]">-</p>
-                                <p className="uppercase text-[20px] font-[400]">{`Thời lượng:(${movie?.duration})`}</p>
+                                <p className="uppercase text-[20px] font-[400]">{`(${movie?.duration})`}</p>
                             </div>
                             <p className="text-center uppercase text-[#ffffff61] text-[16px]">
                                 {formatDate}, {formatTime(showTimeData?.data?.start_time)}
@@ -566,7 +566,7 @@ const SeatBooking = () => {
                                         { bg: '#7331d6', label: 'Ghế thường' },
                                         { bg: '#f14052', label: 'Ghế VIP' },
                                         { bg: '#ffc107', label: 'Ghế Đôi' },
-                                        { bg: '#0dcaf0', label: 'Ghế Đang Giữ' },
+                                        { bg: '#1d59a2', label: 'Ghế Đang Chọn' },
                                     ].map((item, index) => {
                                         return (
                                             <div className="flex justify-start items-center gap-[10px]" key={index}>
